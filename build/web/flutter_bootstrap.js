b4f803d522f134cb7a64fe1524fcfaa411046776
Phea_k <engsopheak532@gmail.com>
2026-02-08 13:15:28 +0700

    upload

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1826327831"
+    serviceWorkerVersion: "1767899304"
   }
 });
